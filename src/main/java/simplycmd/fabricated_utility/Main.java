d1e7e462299b095770dc56a4fbfda63f751e5c56
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-01-21 20:27:53 +1100

    cd

--- src/main/java/simplycmd/fabricated_utility/Main.java
+++ src/main/java/simplycmd/fabricated_utility/Main.java
@@ -3,9 +3,11 @@ package simplycmd.fabricated_utility;
 import io.github.simplycmd.featherlib.registry.ID;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.render.entity.VillagerEntityRenderer;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.item.Item;
@@ -15,6 +17,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 import simplycmd.fabricated_utility.villagers.BetterVillagers;
 import simplycmd.fabricated_utility.villagers.impl.GuiEvents;
 import simplycmd.fabricated_utility.villagers.pickup.VillagerItem;
@@ -23,6 +26,7 @@ import static simplycmd.fabricated_utility.villagers.pickup.VillagerItemRenderer
 
 public class Main implements ModInitializer, ClientModInitializer {
     private VillagerEntityRenderer renderer;
+    public static final KeyBinding PICK_UP_VILLAGER = new KeyBinding("key.easy_villagers.pick_up", GLFW.GLFW_KEY_V, "category.easy_villagers");
     public static final String MOD_ID = "fabricated_utility";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final VillagerItem VILLAGER_ITEM = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "villager_item"), new VillagerItem());
@@ -30,6 +34,7 @@ public class Main implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
+        KeyBindingHelper.registerKeyBinding(PICK_UP_VILLAGER);
         GuiEvents.init();
         BetterVillagers.init();
         LOGGER.info(MOD_ID + " activated :O");
@@ -41,7 +46,7 @@ public class Main implements ModInitializer, ClientModInitializer {
             if (renderer == null) {
                 renderer = new VillagerEntityRenderer(createEntityRendererContext());
             }
-            renderer.render(Main.VILLAGER_ITEM.getVillagerFast(MinecraftClient.getInstance().world, stack), 0F, 1F, matrices, vertexConsumers, light);
+            renderer.render(Main.VILLAGER_ITEM.getVillagerFast(MinecraftClient.getInstance().world, stack), 0F, false ? 1F : 0F, matrices, vertexConsumers, light);
         }));
     }
 }
---
+++ src/main/java/simplycmd/fabricated_utility/villagers/mixin/VillagerEntityMixin.java
@@ -0,0 +1,30 @@
+package simplycmd.fabricated_utility.villagers.mixin;
+
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import simplycmd.fabricated_utility.Main;
+
+@Mixin(VillagerEntity.class)
+public class VillagerEntityMixin {
+    @Inject(method = "interactMob", at = @At("HEAD"))
+    private void interact(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        pickUp((VillagerEntity) (Object) this, player);
+    }
+
+    private static void pickUp(VillagerEntity villager, PlayerEntity player) {
+        ItemStack stack = new ItemStack(Main.VILLAGER_ITEM);
+
+        Main.VILLAGER_ITEM.setVillager(stack, villager);
+
+        if (player.getInventory().insertStack(stack)) {
+            villager.discard();
+        }
+    }
+}
