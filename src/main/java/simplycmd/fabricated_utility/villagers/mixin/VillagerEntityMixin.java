8b5403b8933bf7f02ad1bb274d736054ebfd90cb
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-01-22 13:29:02 +1100

    ~

--- src/main/java/simplycmd/fabricated_utility/villagers/mixin/VillagerEntityMixin.java
+++ src/main/java/simplycmd/fabricated_utility/villagers/mixin/VillagerEntityMixin.java
@@ -5,14 +5,21 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.village.VillagerData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import simplycmd.fabricated_utility.Main;
+import simplycmd.fabricated_utility.villagers.api.BetterVillagersExtension;
 
 @Mixin(VillagerEntity.class)
-public class VillagerEntityMixin {
+public abstract class VillagerEntityMixin implements BetterVillagersExtension {
+    @Shadow public abstract VillagerData getVillagerData();
+
+    @Shadow public abstract boolean canRestock();
+
     @Inject(method = "interactMob", at = @At("HEAD"))
     private void interact(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         if (!player.isSneaking()) return;
@@ -27,5 +34,20 @@ public class VillagerEntityMixin {
         if (player.getInventory().insertStack(stack)) {
             villager.discard();
         }
+    }
+
+    @Override
+    public int betterVillagers$getLevel() {
+        return this.getVillagerData().getLevel();
+    }
+
+    @Override
+    public boolean betterVillagers$canRestock() {
+        return this.canRestock();
+    }
+
+    @Override
+    public boolean betterVillagers$supportsBetterVillagers() {
+        return true;
     }
 }
